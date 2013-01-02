493c0ca42a0790a3d538b32bcf14d8c5ba482033
Novatouch <slurp.miammiam@gmail.com>
2013-01-02 20:56:26 +0100

    Je ne sais pas utiliser Git

--- test/threadSocket/Client/src/connexion/Client.java
+++ test/threadSocket/Client/src/connexion/Client.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
