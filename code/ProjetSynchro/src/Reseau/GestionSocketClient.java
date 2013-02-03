08c6695e360d7aa75f2beae45b917de959e63b37
Novatouch <slurp.miammiam@gmail.com>
2013-02-03 23:12:24 +0100

    Ajout classe package Reseau

---
+++ code/ProjetSynchro/src/Reseau/GestionSocketClient.java
@@ -0,0 +1,46 @@
+package Reseau;
+
+import java.io.IOException;
+
+
+/**
+ * <b>Cette classe regroupe les objets nécessaires à la création d'une connexion du coté client.</b>
+ * <p>
+ * Liste des différents attribus :
+ * <ul>
+ * <li> socketClient </li>
+ * <li> out </li>
+ * <li> in </li>
+ * </ul>
+ * </p>
+ * <p>
+ * 
+ * 
+ * </p>
+ * 
+ * @see GestionSocketClient
+ * 
+ * @author Gautier Philippe
+ * @version 1.0
+ */
+
+public class GestionSocketClient {
+
+	/**
+     * Constructeur GestionSocketClient.
+     * <p>
+     * Lors de la création d'un objet GestionSocketClient la variable socketClient est initialisée et la connexion 
+     * au serveur est effectuée. Les deux variables gérant les flux sont aussi initialisées
+     * </p>
+     * 
+     * @param param1
+     *            Presentation paramètre.
+     * @param parma2
+     *            Le même.
+     * 
+	 * liste des attributs modifiés par le constructeur
+	 * @throws IOException 
+	 * @see GestionSocketServeur#socket
+	 * 
+	 */
+}
---
+++ code/ProjetSynchro/src/Reseau/GestionSocketServeur.java
@@ -0,0 +1,87 @@
+package Reseau;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * <b>Cette classe regroupe les objets nécessaires à la création d'une connexion serveur.</b>
+ * <p>
+ * Liste des différents attribus :
+ * <ul>
+ * <li> socket</li>
+ * </ul>
+ * </p>
+ * <p>
+ * 
+ * 
+ * </p>
+ * 
+ * @see GestionSocketClient
+ * 
+ * @author Gautier Philippe
+ * @version 1.0
+ */
+
+public class GestionSocketServeur {
+	
+	/**
+     * Cette varaible stockeras le socket créer par le serveur
+     */
+	
+	private ServerSocket socketserver;
+
+	/**
+     * Constructeur GestionSocketServeur.
+     * <p>
+     * Lors de la création d'un objet GestionSocketServeur l'attribut socket est initialisé.
+     * </p>
+     * 
+     * @param param1
+     *            Presentation paramètre.
+     * @param parma2
+     *            Le même.
+     * 
+	 * liste des attributs modifiés par le constructeur
+	 * @throws IOException 
+	 * @see GestionSocketServeur#socket
+	 * 
+	 */
+	public GestionSocketServeur() throws IOException{
+		
+		//initialisation socket
+		socketserver = new ServerSocket(5000, 5);
+		System.out.println("INFORMATION: serveur > lancement du serveur sur le port : "+ socketserver.getLocalPort());
+		
+	}
+	
+	/**
+     * Permet d'accepter la connexion d'un client.
+     * 
+     * @return un objet socketclient.
+	 * @throws IOException 
+     * 
+     */
+	public Socket accepterClient() throws IOException {
+		
+		Socket socketClient = new Socket();
+		socketClient = socketserver.accept();
+		
+		return socketClient;
+	}
+
+
+	/**
+     * Permet de fermer la connexion du serveur.
+	 * @throws IOException 
+     * 
+     */
+	public void arreterServeur() throws IOException {
+		
+		socketserver.close();
+	}
+
+
+	
+	
+}
